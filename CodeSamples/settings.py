7b3e8bc6fa2796c2fbf6da2479f070e96a8b398b
Vamsidhar Muggulla <vamsidhar@micropyramid.com>
2016-08-18 11:39:46 +0530

    Update settings.py

--- CodeSamples/settings.py
+++ CodeSamples/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -125,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT='/static/'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), 'CodeSamples/static/', ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), 'fotoblogger/static/', ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
