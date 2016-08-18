983c584f30d12d60808b3f66d8afef154e61b9bb
Vamsidhar Muggulla <vamsidhar@micropyramid.com>
2016-08-18 11:50:47 +0530

    Update settings.py

--- CodeSamples/settings.py
+++ CodeSamples/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT,'fotoblogger')
-STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STARTIC_URL='/static/'
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), 'fotoblogger/static/', ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
